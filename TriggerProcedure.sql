058d92cb8d4b69bf6f100a2290c6f3bd655fc670
Raffaele Ventriglia <raffaeleventriglia6@gmail.com>
2022-07-07 22:06:45 +0200

    UPDATE

--- TriggerProcedure.sql
+++ TriggerProcedure.sql
@@ -1,20 +1,16 @@
 /*
-    Trigger che controlla se, all'inserimento della tupla di un dipendente,
+    trigger 1: controlla se, all'inserimento della tupla di un dipendente,
     questo sia minorenne, e nel caso in cui lo fosse ritornerà l'errore
     'Dipendente minorenne'
 */
 
-CREATE TRIGGER Minorenne
+CREATE OR REPLACE TRIGGER Minorenne
 BEFORE INSERT ON Dipendente
 FOR EACH ROW
 DECLARE
-    DNnascita DATE;
     Check_Minorenne EXCEPTION;
 BEGIN
-    SELECT DNdipendente INTO DNnascita
-    FROM Dipendente
-    WHERE DNnascita = :new.DNdipendente;
-    IF (FLOOR((sysdate - :new.DNdipendente) / 365) < 17)
+    IF (((sysdate - :new.DNdipendente) / 365) < 18)
     THEN
         RAISE Check_Minorenne;
     END IF;
@@ -25,16 +21,51 @@ EXCEPTION
 END;
 
 /*
-    Procedura che controlla se, dopo l'inserimento della tupla di una tessera,
-    siano presenti almeno 50 punti, e nel caso in cui ci fossero allora il cliente
-    riceve uno sconto sullo scontrino di Euro 20
+    trigger 2: il dipendente non può iniziare un nuovo turno o presenza, senza che
+               quello precedente sia finito
 */
 
-CREATE OR REPLACE PROCEDURE PuntiTessera (SogliaPunti NUMBER) IS
-CURSOR PROVA IS
-SELECT Punti
-    FROM Tessera
-    WHERE Punti >= 50;
+/*
+    trigger 3: il dipendente non può effettuare più di 9 ore di lavoro, considerando
+               anche l'ora di pranzo
+*/
+
+CREATE OR REPLACE TRIGGER Ore_Lavoro
+BEFORE INSERT ON Presenza
+FOR EACH ROW
+DECLARE
+    Durata_Turno NUMBER;
+    Check_Ore_Lavoro EXCEPTION;
 BEGIN
-    
+    SELECT EXTRACT(HOUR FROM :new.UltimaOra - :new.PrimaOra) INTO Durata_Turno FROM Presenza;
+    IF Durata_Turno > 9
+    THEN
+        RAISE Check_Ore_Lavoro;
+    END IF;
+EXCEPTION
+    WHEN Check_Ore_Lavoro
+    THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Ore di lavoro superate');
 END;
+
+/*
+    trigger 4: per una politica aziendale, il negozio chiude alle ore 21, e quindi
+               la presenza di un dipendente non può andare oltre quest'orario
+
+
+    trigger 5: solo i cassieri e i magazzinieri hanno accesso al portale del magazzino,
+               quindi uno scaffalista non ha credenziali di accesso
+
+
+    trigger 6: 
+*/
+
+
+/*
+
+    procedura 1: promuove l'impiegato che lavora di più
+
+
+    procedura 2: converte i soldi spesi in punti tessera
+
+*/
