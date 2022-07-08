bcfd6f76e523bb463210c4b1d7eeeba32c7d215d
Raffaele Ventriglia <raffaeleventriglia6@gmail.com>
2022-07-08 17:50:18 +0200

    UPDATE

--- TriggerProcedure.sql
+++ TriggerProcedure.sql
@@ -1,9 +1,9 @@
--- TRIGGER
+--------------------- TRIGGER
 
 /*
     trigger 1: controlla se, all'inserimento della tupla di un dipendente,
     questo sia minorenne, e nel caso in cui lo fosse ritornerà l'errore
-    'Dipendente minorenne'
+    'Dipendente minorenne'. Se è troppo anziana non può lavorare.
 */
 
 CREATE OR REPLACE TRIGGER Minorenne
@@ -13,19 +13,35 @@ DECLARE
     Check_Minorenne EXCEPTION;
 BEGIN
     IF (((sysdate - :new.DNdipendente) / 365) < 18)
-    THEN
-        RAISE Check_Minorenne;
+    THEN RAISE Check_Minorenne;
     END IF;
 EXCEPTION
     WHEN Check_Minorenne
-    THEN
-        RAISE_APPLICATION_ERROR(-20001, 'Dipendente minorenne');
+    THEN RAISE_APPLICATION_ERROR(-20001, 'Dipendente minorenne');
 END;
 
 /*
     trigger 2: il dipendente non può iniziare un nuovo turno o presenza, senza che
                quello precedente sia finito
 */
+
+CREATE OR REPLACE TRIGGER Controllo_Turno
+BEFORE INSERT ON Presenza
+FOR EACH ROW
+DECLARE
+    Turno presenza.ULtimaOra%Type;
+    Check_Turno EXCEPTION;
+BEGIN
+    SELECT MAX(UltimaOra) INTO Turno FROM presenza where CFDipendente = :new.CFDipendente;
+    IF :new.UltimaOra < :new.PrimaOra
+    THEN raise Check_Turno;
+    ELSIF Turno > :new.PrimaOra
+    THEN raise Check_Turno;
+    END IF;
+EXCEPTION
+    WHEN Check_Turno
+    THEN RAISE_APPLICATION_ERROR(-20001, 'Errore nei turni inseriti');
+END;
 
 /*
     trigger 3: il dipendente non può effettuare più di 9 ore di lavoro, considerando
@@ -36,17 +52,15 @@ CREATE OR REPLACE TRIGGER Ore_Lavoro
 BEFORE INSERT ON Presenza
 FOR EACH ROW
 DECLARE
-    Durata_Turno NUMBER := EXTRACT(HOUR FROM :new.UltimaOra - :new.PrimaOra);
+    Durata_Turno NUMBER := (cast(:new.UltimaOra as date) - cast(:new.PrimaOra as date)) * 24 * 60;
     Check_Ore_Lavoro EXCEPTION;
 BEGIN
-    IF Durata_Turno > 9
-    THEN
-        RAISE Check_Ore_Lavoro;
+    IF Durata_Turno > 540
+    THEN RAISE Check_Ore_Lavoro;
     END IF;
 EXCEPTION
     WHEN Check_Ore_Lavoro
-    THEN
-        RAISE_APPLICATION_ERROR(-20001, 'Ore di lavoro superate');
+    THEN RAISE_APPLICATION_ERROR(-20001, 'Ore di lavoro superate');
 END;
 
 /*
@@ -63,26 +77,43 @@ DECLARE
     Check_Apertura_Chiusura EXCEPTION;
 BEGIN
     IF (InizioTurno < 9 OR InizioTurno > 20) OR (FineTurno > 21 OR FineTurno <= 9)
-    THEN
-        RAISE Check_Apertura_Chiusura;
+    THEN RAISE Check_Apertura_Chiusura;
     END IF;
 EXCEPTION
     WHEN Check_Apertura_Chiusura
-    THEN
-        RAISE_APPLICATION_ERROR(-20001, 'Orari di inizio o fine turno errati');
+    THEN RAISE_APPLICATION_ERROR(-20001, 'Orari di inizio o fine turno errati');
 END;
 
 /*
-    trigger 5: solo i cassieri e i magazzinieri hanno accesso al portale del magazzino,
+    trigger 5: solo i cassieri, i magazzinieri e l'amministratore hanno accesso al portale del magazzino,
                quindi uno scaffalista non ha credenziali di accesso
+*/
 
+CREATE OR REPLACE TRIGGER AccessoPortale
+BEFORE INSERT ON Dipendente
+FOR EACH ROW
+DECLARE
+    Check_Accesso_Portale EXCEPTION;
+BEGIN
+    IF :new.Ruolo = 'Scaffalista' AND :new.Username IS NOT NULL
+    THEN RAISE Check_Accesso_Portale;
+    END IF;
+EXCEPTION
+    WHEN Check_Accesso_Portale
+    THEN RAISE_APPLICATION_ERROR(-20001, 'Il dipendente non ha accesso al portale');
+END;
 
-    trigger 6: 
+/*
+    trigger 6: non è possibile vendere prodotti in eccesso alla disponibilità
+
+    trigger 7: controllare se lo stipendio è sufficiente in base al ruolo del dipendente
+
+    trigger 8: 
 */
 
 
 
--- PROCEDURE
+--------------------- PROCEDURE
 
 /*
 
